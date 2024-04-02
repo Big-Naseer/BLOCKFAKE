39d62ddd1266e914e9ed2fed85eed1135dbbe814
Big-Naseer <nasiruyusuf59@outlook.com>
2024-04-02 07:13:37 -0400

    modified admin route

--- src/route/admin/admin.route.js
+++ src/route/admin/admin.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllAdmins } from "../../controller/admin.controller.js ";
+import { getAllAdmins } from "../../controller/admin.controller .js";
 import { adminLogin } from "../../controller/auth.controller.js";
 
 const router = express.Router()
